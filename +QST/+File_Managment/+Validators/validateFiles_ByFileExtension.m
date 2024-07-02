a04555e2b923b66ad76bde5fe28c332d13fa7ae4
Yannik Brune <yannik.brune@tu-dortmund.de>
2024-07-02 16:07:03 +0200

    reorganized the structure a bit

--- +QST/+File_Managment/+Validators/validateFiles_ByFileExtension.m
+++
@@ -1,9 +0,0 @@
-function [isValid,Files] = validateFiles_ByFileExtension(Files,Extension)
-%Function that returns a list of all files that have a matching file
-%extension
-
-[~, ~, Ext] = fileparts(Files);
-isValid = contains(Ext,Extension);
-Files = Files(isValid);
-end
-
