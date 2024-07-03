2fe39221c2b79bb09120cae631c3f42f38ebac00
Yannik Brune <yannik.brune@tu-dortmund.de>
2024-07-03 17:24:03 +0200

    I changed the names of almost all functions so they share the same logic. Unfortuanetly some function names are now quite weird and iam not sure if they stay like this.

--- +QST/+Simulation/+QuantumCoherence/coherencePDTS.m
+++
@@ -1,9 +0,0 @@
-function Coherence = coherencePDTS(nCoherent,nTherm)
-% computes coherence of a phase-averaged displaced thermal state.
-% nTherm: thermal photon number
-% nCoherent: coherent photon number
-%     a = 2*nTherm + 1;
-    a = (nTherm + 1).^2 - nTherm.^2;
-    Coherence = (1 - exp(-2*nCoherent./a) .* besseli(0,2*nCoherent./a ) )./a;
-end
-
