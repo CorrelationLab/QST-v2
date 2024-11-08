0470d06654c3b11af5a2b6565779108d2d67c88e
Yannik_Laptop <yannik.brune@tu-dortmund.de>
2024-11-08 22:02:28 +0100

    Added an optimized function to calculate the projections for the density matrix reconstruction. scales linear instead of quadratic with fock space dimension. can be further upgraded to use a gpu.

--- +QST/+DensityMatrix/computeDensityMatrix_FAST.m
+++ +QST/+DensityMatrix/computeDensityMatrix_FAST.m
@@ -22,7 +22,7 @@ clear Xtp;
 nX = length(X);
 nextRho = gpuArray(single(ones(Options.MaxFockState+1,Options.MaxFockState+1)));
 nextRho = nextRho /trace(nextRho);
-A = gpuArray(single(QST.DensityMatrix.computeProjector1D( X, Theta, Options.MaxFockState)));
+A = gpuArray(single(QST.DensityMatrix.computeProjector1D_FAST( X, Theta, Options.MaxFockState)));
 B = A';
 tic
 for iRho = 1:Options.Iterations
